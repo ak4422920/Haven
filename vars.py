233aba5ce7b3f2d040fa168f8b8b217454dd88da
ak4422920 <ak4422920@gmail.com>
2025-11-04 14:36:34 +0530

    Update AUTH_CHANNELS default values

--- vars.py
+++ vars.py
@@ -11,6 +11,6 @@ PICS = (os.environ.get("PICS", "https://i.ibb.co/vvXmVg7f/photo-2025-08-19-14-15
 LOG_CHNL = int(os.getenv("LOG_CHNL", ""))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "NullZoro") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", True))
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1001785093771 -1002827553840").split()))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1001785093771 -1002779576540").split()))
 DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_LOG", ""))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "600"))
